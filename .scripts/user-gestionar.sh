4b1fa0f96235bfb0296c150a77a7979fa8d3344f
root <root@smx2.org>
2019-02-05 10:37:02 +0000

    añadida lista de usuarios en menú. Mejorado borrado de usuarios del sistema

--- .scripts/user-gestionar.sh
+++ .scripts/user-gestionar.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 OP=0
 
+bo=$(tput bold)
+no=$(tput sgr0)
+
 # Run as root, of course.
 
 if [ "$UID" -ne "0" ]
@@ -14,6 +17,9 @@ clear
 echo "-------------------------"
 echo Menu
 echo "-------------------------"
+printf "Usuarios\n\n"
+cat /etc/passwd | grep home | cut -f 1,6 -d : 
+printf "\n\n"
 echo "[q] Salir"
 echo "[1] Usuarios con /home"
 echo "[2] Borra usuario"
@@ -27,37 +33,55 @@ case $OP in
         cut -f 1,6 /etc/passwd -d : | grep home | sort
         read parar
         ;;
-    2)  printf "Indica el nombre del usuario a borrar"
-        read -p "Nombre del usuario a borrar: `echo $'\n> '`" nombre
-        userdel $nombre > /dev/null
+    2)  read -p "Nombre del usuario a borrar: `echo $'\n> '`" nombre
+        read -p "Borrar carpeta home [s/n]: `echo $'\n> '`" borrarhome
+        if [ "$borrarhome" == "s" ]
+	then
+		userdel -r $nombre 2>/dev/null
+		printf "Home de ${bo}$nombre${no} borrada\n"
+	else
+		userdel $nombre 2> /dev/null
+		printf "La home de ${bo}$nombre${no} no se borrará\n"
+	fi
 	if [ $? -eq 6 ]
 	then
-		echo El usuario no está registrado en el sistema
+		echo "El usuario no está registrado en el sistema\n"
 	else
-        	echo Usuario borrado
+        	printf "Usuario borrado\n"
         fi
-	read parar
+	read -p "pulsa una tecla para continuar .." parar
 	;;
     3)  echo "Creación de usuario:"
         read -p "Nombre del usuario a crear: `echo $'\n> '`" nombre
-        read -p "Rol del usuario (A o P): `echo $'\n> '`" rol
-        if [ "$rol" == "A" ]
-        then
-                tipouser="alumnos"
-        elif [ "$rol" == "P" ]
-        then
-                tipouser="profesores"
-        else
-                exit
-        fi
-        if [ -d "$DIRECTORY" ]; then mkdir /home/$tipouser; fi
-        useradd -m -d /home/$tipouser/$nombre $nombre -s /bin/bash
-        passwd $nombre
-        echo Usuario: $nombre
-        echo Home: /home/$tipouser/$nombre
-        echo Grupos:
-        grep $nombre /etc/group | cut -f 1 -d :
-        ;;
+        if id "$nombre" > /dev/null 2>&1; then
+		echo "el usuario ${bo}$nombre${no} ya existe"
+	else
+		read -p "Rol del usuario (A o P): `echo $'\n> '`" rol
+        	if [ "$rol" == "A" ]
+        	then
+                	tipouser="alumnos"
+        	elif [ "$rol" == "P" ]
+        	then
+                	tipouser="profesores"
+        	else
+                	exit
+        	fi
+		home_grupo="/home/$tipouser/"
+
+		#Creamos carpeta de grupo di no existia en home
+		if [ ! -d "$home_grupo" ]
+		then
+			mkdir $home_grupo; 
+		fi
+        	useradd -m -d /home/$tipouser/$nombre $nombre -s /bin/bash
+        	passwd $nombre
+        	echo Usuario: $nombre
+        	echo Home: /home/$tipouser/$nombre
+        	echo Grupos:
+        	grep $nombre /etc/group | cut -f 1 -d :
+	fi
+	read parada
+	;;
      4) lastlog | grep -v **Never
         read parada
         ;;
